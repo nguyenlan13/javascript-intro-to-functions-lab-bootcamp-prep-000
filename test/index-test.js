3696b712f27f44905327bc8a6c5fa397427d5366
Lan Nguyen <nguyenlan13@gmail.com>
2019-01-28 04:10:19 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -59,7 +59,7 @@ describe('logWhisper(string)', function() {
 function sayHiToGrandma(string){
  if (string === string.toLowerCase()) return "I can\'t hear you!";
   else if (string === string.toUpperCase()) return "YES INDEED!";
-  else if (string === "I love you, Grandma."()) return "I love you, too.";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
   
 }
 
