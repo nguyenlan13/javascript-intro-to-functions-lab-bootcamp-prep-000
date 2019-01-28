3a9e1b4d5dd3a5a958a14685a6c1b214805d8ef7
Lan Nguyen <nguyenlan13@gmail.com>
2019-01-28 03:54:10 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -38,6 +38,11 @@ describe('logShout(string)', function() {
   })
 })
 
+
+function logWhisper (string){
+  console.log (string.toLowerCase())
+}
+
 describe('logWhisper(string)', function() {
   it('calls console.log() its one argument in all lowercase', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
