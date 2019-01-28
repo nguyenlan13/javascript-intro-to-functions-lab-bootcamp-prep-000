178544a50c4154e8cf3af8c47ba19b3efa94a6dc
Lan Nguyen <nguyenlan13@gmail.com>
2019-01-28 03:23:15 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,15 +1,30 @@
 
+function shout(string){
+  return string.toUpperCase()
+}
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
 
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
   })
 })
+
+function logShout(string){
+  console.log (string.toUpperCase)()
+  require 
+}
+function
+
 
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
